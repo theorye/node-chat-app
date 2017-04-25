675d90fe229941224bca2b39d227dbb3cbff3b5e
Adrian H <limitmaker@Adrians-MacBook-Air.local>
2017-04-25 12:13:38 -0500

    Add user class and update user list event

---
+++ public/js/chat.js
@@ -0,0 +1,118 @@
+var socket = io();
+
+function scrollToBottom() {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    // console.log('Should scroll')
+    messages.scrollTop(scrollHeight)
+  }
+
+}
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+  console.log(params);
+  socket.emit('join', params, function(err) {
+    console.log('here')
+    if(err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No Error')
+    }
+  });
+});
+
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user))
+  })
+
+  jQuery('#users').html(ol);
+})
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
+  // var li = jQuery('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  //
+  // jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My current location</a>');
+  //
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  var messageTextbox = jQuery('[name=message]');
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: messageTextbox.val()
+  }, function () {
+    messageTextbox.val('')
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location.');
+  });
+});
--- server/server.js
+++ server/server.js
@@ -4,21 +4,20 @@ const express = require('express');
 const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 
+const {isRealString} = require('./utils/validation');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {Users} = require('./utils/users');
 let app = express();
 const port = process.env.PORT || 3000;
 var server = http.createServer(app)
 var io = socketIO(server);
-
+var users = new Users();
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new user connected');
   // socket.emit from Admin text Welcome to the chat app
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-  // socket.broadcast.emit from Admin text New user joined
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
-  // socket.emit('newEmail',{
+
   //   from: 'mike@example.com',
   //   text: 'hey what is going ',
   //   createAt: 123
@@ -28,6 +27,27 @@ io.on('connection', (socket) => {
   //   text: 'see you then',
   //   createdAt: 12343
   // })
+  socket.on('join', (params, callback) => {
+    if(!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required');
+    }
+
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    // io.emit -> io.to('The Office Fan').emit
+    // socket.broadcast.emit -> socket.broadcast.to('').emit
+    // socket.emit
+    //
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    // socket.broadcast.emit from Admin text New user joined
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+    // socket.emit('newEmail',{
+
+    callback();
+  });
 
   // socket.on('createEmail', (newEmail) => {
   //   console.log('createEmail', newEmail);
@@ -54,6 +74,12 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
+    var user = users.removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+    }
   })
 
 })
